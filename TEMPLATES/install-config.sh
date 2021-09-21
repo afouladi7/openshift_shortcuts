dbc6d9e7340619b6ba34fbe8bb4b6307ff52739f
afouladi7 <allenfouladi@gmail.com>
2021-09-21 11:42:22 -0400

    adding install config

---
+++ TEMPLATES/install-config.sh
@@ -0,0 +1,60 @@
+echo "Please enter your pull secret from cloud.redhat.com/openshift/install in a file called pull.txt"
+sleep 5
+read -p "Please enter your sshkey public key: " key
+pull=$(<pull.txt)
+
+cat << EOF > ./install-config.yaml
+apiVersion: v1
+baseDomain: allenfouladi.com
+compute:
+- architecture: amd64
+  hyperthreading: Enabled 
+  name: worker
+  platform:
+    aws:
+      zones:
+      - us-east-1a
+      - us-east-1b
+      - us-east-1c
+      rootVolume:
+        iops: 2000
+        size: 500
+        type: io1 
+      type: m5a.4xlarge
+  replicas: 3
+controlPlane:
+  architecture: amd64
+  hyperthreading: Enabled 
+  name: master
+  platform:
+    aws:
+      zones:
+      - us-east-1a
+      - us-east-1b
+      - us-east-1c
+      rootVolume:
+        iops: 2000
+        size: 500
+        type: io1 
+      type: m5a.2xlarge
+  replicas: 3
+metadata:
+  creationTimestamp: null
+  name: testcluster
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  machineNetwork:
+  - cidr: 10.0.0.0/16
+  networkType: OpenShiftSDN
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  aws:
+    region: us-east-1
+publish: External
+pullSecret: '$pull'
+sshKey: |
+  $key
+EOF
